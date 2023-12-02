853fc0f8056adb06b205e3971bdfe1842ecb4e07
Grace Sandford <gracetree3@gmail.com>
2023-12-02 20:30:37 +0000

    task one done

--- Day1/clean_data.py
+++ Day1/clean_data.py
@@ -1,4 +1,21 @@
-class CleanData():
-  def read():
-    f = open("./Day1Data.txt", "r")
-    return f.read()
+import re
+
+def doubleNumber(num):
+  doubled = [num[0], num[0]]
+  return doubled
+
+def lastFirst(nums):
+  lastf = [nums[0], nums[-1]]
+  return lastf
+
+def concat(digits):
+  return digits[0] + digits[-1]
+
+def getFirstLastNum(line):
+  numbers = re.findall(r"\d", line)
+  if(len(numbers) == 1):
+    return doubleNumber(numbers)
+  if(len(numbers) > 2):
+    return lastFirst(numbers)
+  return numbers
+  
