995e84fa56c5b3e407f00eef89c71f72d1b09284
Grace Sandford <gracetree3@gmail.com>
2023-12-05 16:58:12 +0000

    refactor to task 2

--- Day1/test_day_one.py
+++ Day1/test_day_one.py
@@ -5,11 +5,7 @@ from clean_data import getFirstLastNum, concat
 
 @pytest.fixture
 def read_data():
-    return open("Day1/day1Data.txt", "r")
-
-def first(line):
-    m = re.search(r"\d", line)
-    return re.search(r"\d", line).group()
+    return open("Day1/word_number_example.txt", "r")
 
 def test_day_one(read_data):
     lines = read_data.readlines()
@@ -22,10 +18,10 @@ def test_day_one(read_data):
         assert len(numbers) != 0
         assert len(numbers) != 1
         assert len(numbers) <= 2
-        assert numbers[0] == first(line)
         cleanNumber = concat(numbers)
         assert cleanNumber != (int(numbers[0]) + int(numbers[-1]))
         sumNoList.append(int(cleanNumber))
     
     total = sum(sumNoList)
-    assert total != 0
+    assert total == 281
+    print(total)
