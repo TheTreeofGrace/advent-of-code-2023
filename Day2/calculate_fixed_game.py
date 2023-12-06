2b89acf8bd8bd09dcaa9ec7052a21a2cb6b62a99
Grace Sandford <gracetree3@gmail.com>
2023-12-06 18:38:28 +0000

    refactor

--- Day2/calculate_fixed_game.py
+++ Day2/calculate_fixed_game.py
@@ -26,26 +26,16 @@ class CalcPossible():
 
     def process_value(self, cubes):
         for cube in cubes:
-            print(cube)
             self.game_cubes[cube[3]] = self.game_cubes[cube[3]] + int(cube[2])
-            print(self.game_cubes[cube[3]])
-        print(self.game_cubes)
 
     def get_game_id(_, line):
         return re.search(r"(?!(Game ))\d+", line).group()
 
     def calculate_possible(self, line):
-        print(line)
         gameID = self.get_game_id(line)
         cubes = self.get_cubes(line)
         self.process_value(cubes)
-        is_impossible = False
         for val in max_colours:
-            print(max_colours[val])
-            print(self.game_cubes)
             if(max_colours[val] < self.game_cubes[val]):
-                print(self.game_cubes[val])
-                is_impossible = True
-        if(is_impossible):
-            return 0
+                return 0
         return int(gameID)
